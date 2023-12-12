e854e4accaf4d59f501cba550246ac7f625af426
InnerBalance <123519435+InnerBalance@users.noreply.github.com>
2023-12-11 17:48:35 -0800

    Update Analytics.jsx

--- web/src/components/Analytics/Analytics.jsx
+++ web/src/components/Analytics/Analytics.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Analytics() {
   return (
      <div>
-     <a title="hit counter" href="https://statcounter.com/" target="_blank">
-       <img src="https://c.statcounter.com/12916898/0/9e9b5e41/1/" alt="hit counter">
+       <a title="hit counter" href="https://statcounter.com/" target="_blank">
+         <img src="https://c.statcounter.com/12916898/0/9e9b5e41/1/" alt="hit counter"/>
        </a>
      </div>
   );
